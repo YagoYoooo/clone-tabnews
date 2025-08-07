5cb9e76b542831de5ce67829ffe04e7cee0d6624
YagoYoooo <yagoalvesdejesus@outlook.com>
2025-08-07 20:39:16 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Olá</h1>
+    return <h1>Oi, amor fiz isso para dizer que eu te amo e que eu estou estudando e não apenas vagabundando 😎</h1>
 }
 
 export default Home;
