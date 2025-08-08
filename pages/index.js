eb9b9583cbdf9e04da746c578dd729fcee3f8b57
YagoYoooo <yagoalvesdejesus@outlook.com>
2025-08-08 00:49:39 +0000

    adciona scripts `Lint:check` e `Lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Gosta de permissão, Yes or Não?</h1>
+  return <h1>Gosta de permissão, Yes or Não?</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
